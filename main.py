5cb447b94026f76d921f032107d07ff98701bcc2
Daylam Tayari <git@tayari.gg>
2025-10-27 21:08:52 -0500

    Update logging output format

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 from display import TransitDisplay
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 
