0b9a90ea6749cf327dce598a3e4beac7ad9e9c89
Daylam Tayari <git@tayari.gg>
2026-01-07 10:20:12 -0600

    Fixed minutes until eta not being recursive

--- utils.py
+++ utils.py
@@ -29,16 +29,16 @@ def minutes_until_date(date_timestamp):
     return floor(delta.total_seconds() / 60)
 
 
-def minutes_until_eta(eta):
+def minutes_until_eta(eta, i=0):
     """
     Returns the amount of minutes until an arrival, using the following
     arrival if the time until the arrival is under the configured cutoff,
     unless there is no additional arrival
     """
     cutoff = int(secrets.get("MINUTES_UNTIL_ETA_CUTOFF", 5))
-    minutes = minutes_until_date(eta[0])
+    minutes = minutes_until_date(eta[i])
     logger.debug(f"Minutes until ETA: {minutes}")
     # If time is under cutoff and there's another arrival, show time until next arrival instead
     if minutes < cutoff and len(eta) > 1:
-        return minutes_until_date(eta[1])
+        return minutes_until_date(eta[i+1])
     return minutes
